7cf70e17cb2aab4b0ea6ea2d5ca5a63be65e15bf
muk378 <muk7859@hotmail.com>
2019-04-17 16:02:43 +0700

    cleansing project

--- routes/reportpage.js
+++ routes/reportpage.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var RepairSystem = require('../models/repairsystem');
+
 /* GET contact page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  res.render('reportpage');
+});
+
+router.post('/', function (req, res, next) {
+  console.log('params: ', req.params);
+  if (req.params) {
+    var repair = new RepairSystem();
+
+    // repair.repairType =
+    //   repair.repairStatus:
+    // repair.description:
+    // repair.createTime:
+    // repair.updateTime:
+    // repair.updateBy:  // userName
+
+    // save the user
+    // repair.save(function (err) {
+    //   if (err)
+    //     throw err;
+    //   return done(null, repair);
+    // });
+  }
+
   res.render('reportpage');
 });
 
